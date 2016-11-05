077f26c9a8913c3fd6ed72ea9aba40b1a7592cac
coreytegeler <coreytegeler@gmail.com>
2016-11-04 23:57:58 -0400

    content

--- assets/coffee/script.coffee
+++ assets/coffee/script.coffee
@@ -1,19 +1,20 @@
 $ ->
 	transEnd = 'transitionend webkitTransitionEnd oTransitionEnd otransitionend MSTransitionEnd'
 	$body = $('body')
+	$main = $('main')
 	$curtain = $('#curtain')
 	$stairs = $('#stairs')
-	$body.on 'mousewheel', (e) ->
-		if($stairs.is('.open'))
-			return false
-		y = parseInt($curtain.css('y')) - e.deltaY
-		if y >= $(this).innerHeight()
-			y = $(this).innerHeight()
-		if y <= 0
-			y = 0
-		$curtain.transition
-			y: y
-		, 0
+	# $body.on 'mousewheel', 'main', (e) ->
+	# 	if($stairs.is('.open'))
+	# 		return false
+	# 	y = parseInt($curtain.css('y')) - e.deltaY
+	# 	if y >= $(this).innerHeight()
+	# 		y = $(this).innerHeight()
+	# 	if y <= 0
+	# 		y = 0
+	# 	$curtain.transition
+	# 		y: y
+	# 	, 0
 
 	$body.on 'click', '.stair a', (e) ->
 		e.preventDefault()
@@ -22,10 +23,12 @@ $ ->
 			$project.removeClass('open')
 			$stairs.removeClass('open')
 		else
+			$('.project.open').removeClass('open')
 			$project.addClass('open')
 			$stairs.addClass('open')
 			$curtain.css
 				y: $(window).innerHeight()
 			projectTop = $project.position().top
-			$stairs.scrollTop(projectTop-30)
+			scrollTop = $stairs.scrollTop()
+			$stairs.scrollTop(scrollTop + projectTop - 31).addClass('noScroll')
 		
--- site/snippets/stairs.php
+++ site/snippets/stairs.php
@@ -6,13 +6,43 @@ echo '<div id="stairs">';
 	$size = sizeof( $projects );
 	foreach( $projects as $project )	{
 		$margin = ( $size - $i ) * 50;
+		$tags = $project->tags()->split( ',' );
+		$materials = $project->materials()->split( ',' );
 		echo '<div class="project" style="margin-left:' . $margin . 'px">';
 			echo '<div class="stair">';
 				echo '<a href="' . $project->url() . '">';
 					echo $project->title();
 				echo '</a>';
 				echo '<div class="content">';
-
+					echo '<div class="block">';
+						echo $project->text()->kirbytext();
+						echo $project->text()->kirbytext();
+						echo $project->text()->kirbytext();
+					echo '</div>';
+					echo '<div class="block tags">';
+						echo 'Tags: ';
+						foreach( $tags as $ii => $tag ) {
+							echo $tag;
+							if( $ii < sizeof( $tags ) ) {
+								echo ', ';
+							}
+						}
+						if( !sizeof( $tags ) ) {
+							echo '<em>None yet</em>';
+						}
+					echo '</div>';
+					echo '<div class="block materials">';
+						echo 'Materials: ';
+						foreach( $materials as $iii => $material ) {
+							echo $material;
+							if( $iii < sizeof( $materials ) ) {
+								echo ', ';
+							}
+						}
+						if( !sizeof( $materials ) ) {
+							echo '<em>None yet</em>';
+						}
+					echo '</div>';
 				echo '</div>';
 				echo '<div class="gap"></div>';
 			echo '</div>';
